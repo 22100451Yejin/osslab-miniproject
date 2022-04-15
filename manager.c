fa8e3a038ea0cac7969c94b024be3bbf825462c2
22100451Yejin <22100451@handomg.ac.kr>
2022-04-15 17:38:34 +0900

    Stored function definiton

---
+++ manager.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include "manager.h"
+
+
+int readProduct(Product p){
+  printf("%s  %s  %s  %d  %d\n",p.name,p.explain,&p.weight,p.price,p.delivery);
+    return 1;
+    
+}//하나의 제품을 출력하는 함수
+
+
+int listProduct(Product *p, int index){
+
+  printf("제품명  설명  중량  판매가격  배송방법\n");  
+  for(int i=0;i<index;i++){
+        printf("[%d]\n",i+1);
+        readProduct(p[i]);
+    }
+    return 1;
+}
+
+void searchname(Product *p, int count){
+    char search[20];
+    printf("검색할 제품의 이름을 입력하세요: ");
+    scanf("%s",search);
+
+    for(int i=0;i<count;i++){
+        if(strstr(p[i].name,search)){
+            readProduct(p[i]);
+        }
+    }
+} //제품 이름을 검색하는 함수
+
+
+void searchprice(Product *p, int count){
+    int search;
+    printf("검색할 제품의 가격을 입력하세요: ");
+    scanf("%d",&search);
+
+    for(int i=0;i<count;i++){
+        if(p[i].price == search){
+           readProduct(p[i]);
+        }
+    }
+} //제품 가격을 검색하는 함수
+
+
+void searchdelivery(Product *p, int count){
+    int search;
+    printf("검색할 제품의 배송 방법을 입력하세요: ");
+    scanf("%d",&search);
+
+    for(int i=0;i<count;i++){
+        if(p[i].delivery == search){
+            readProduct(p[i]);
+        }
+    }
+} //제품 배송 방법을 검색하는 함수
+
+void saveData(Product *p, int count){
+    FILE *fp;
+    fp=fopen("product.txt","wt");
+
+    for(int i=0;i<count;i++){
+        fprintf(fp,"제품명:%s 설명:%s 중량:%s 판매가격:%d 배송방법:%d\n",p[i].name,p[i].explain,&p[i].weight,p[i].price,p[i].delivery);
+    }
+
+    fclose(fp);
+} //FILE에서 데이터를 불러오는 함수
